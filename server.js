6fcddbba21d0a207f1d2c98793b8e008a7df3e2b
Olalekan Akintola <akinlekan28@yahoo.com>
2021-02-22 15:17:50 +0100

    added bulk notification endpoint

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.post("/", async (req, res) => {
       to: req.body.token,
       title: req.body.title,
       body: req.body.message,
+      data: req.body.data,
       priority: "high",
       sound: "default",
       channelId: "default",
@@ -41,6 +42,47 @@ app.post("/", async (req, res) => {
   };
 
   await sendChunks();
+});
+
+app.post("/broadcast", async (req, res) => {
+  let messages = [];
+  for (let pushToken of req.body.tokens) {
+    // Check that all your push tokens appear to be valid Expo push tokens
+    if (!Expo.isExpoPushToken(pushToken.token)) {
+      console.error(
+        `Push token ${pushToken.token} is not a valid Expo push token`
+      );
+      continue;
+    }
+
+    messages.push({
+      to: pushToken.token,
+      title: req.body.title,
+      body: req.body.message,
+      priority: "high",
+      sound: "default",
+      channelId: "default",
+    });
+  }
+
+  let chunks = expo.chunkPushNotifications(messages);
+  let tickets = [];
+  (async () => {
+    // Send the chunks to the Expo push notification service. There are
+    // different strategies you could use. A simple one is to send one chunk at a
+    // time, which nicely spreads the load out over time:
+    for (let chunk of chunks) {
+      try {
+        let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        console.log(ticketChunk);
+        tickets.push(...ticketChunk);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
+  res.status(200).send({ message: "Push notification sent!" });
+  return;
 });
 
 const server = app.listen(
